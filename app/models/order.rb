2c477049fb6c2dc1894dd414e36bbf046782ce41
nana <sss1230871997@gmail.com>
2024-08-14 22:35:58 +0900

    refile3

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,8 +5,8 @@ class Order < ApplicationRecord
   accepts_nested_attributes_for :ordered_lists
 
   def update_total_quantity
-    self.ordered_lists.each do |line_item|
-      item = Item.lock.find_by(id: line_item.item_id)
+    self.ordered_lists.lock.each do |line_item|
+      item = Item.find_by(id: line_item.item_id)
       item.total_quantity += line_item.quantity
       item.save!
     end
